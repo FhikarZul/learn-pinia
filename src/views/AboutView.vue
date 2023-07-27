bdffc79ff33414aa599d4729d3efab683a53cbc8
Fhikar Zul <Fhikarzul18@gmail.com>
2023-07-27 17:47:32 +0800

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>{{ storeCounter.count }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from "@/stores/counter";
+
+const storeCounter = useCounterStore();
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="count">{{ storeCounter.count }}</div>
+  <div class="count">{{ storeCounter.doubleCount }}</div>
+  <div class="buttons">
+    <button @click="storeCounter.decrement" type="button">-</button>
+    <button @click="storeCounter.increment" type="button">+</button>
+  </div>
+  <input type="number" v-model="storeCounter.count" />
+</template>
+
+<script setup>
+import { useCounterStore } from "@/stores/counter";
+
+const storeCounter = useCounterStore();
+</script>
